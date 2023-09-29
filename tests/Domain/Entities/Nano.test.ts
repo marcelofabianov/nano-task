4f5b2793ffdbc8c7158325f11943f6ba33583518
Marcelo Fabiano <marcelofabianov@gmail.com>
2023-09-29 17:15:04 -0300

    chore: inicio da configuracao ambiente de teste

---
+++ tests/Domain/Entities/Nano.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from "bun:test";
+
+describe('Testes para a entidade Nano', () => {
+  it('Deve criar uma nova instancia com os valores passados', () => {
+    expect(true).toBe(true);
+  });
+});
